cb996e08a590776fbe5a7ad918b6bbdd1a06e4b8
arthur <reaumurab@gmail.com>
2022-06-12 12:17:05 +0200

    ajout du controller StudentHomeController + debut de commande (test)

--- src/main/java/controllers/StudentHomeController.java
+++ src/main/java/controllers/StudentHomeController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
+import models.Student;
 
 public class StudentHomeController {
 
@@ -16,4 +17,11 @@ public class StudentHomeController {
 
     @FXML
     private ChoiceBox groupChoiceBox = new ChoiceBox(FXCollections.observableArrayList("Promo","TD","TP"));
+
+    Student student = new Student();
+
+    String selectStmt = "SELECT * FROM etudiant";
+
+
+
 }
