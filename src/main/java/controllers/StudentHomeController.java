7fa784c9e6e030adaa0f71f81f98b6fdc2dead40
arthur <reaumurab@gmail.com>
2022-06-12 14:24:17 +0200

    ajout du controller StudentHomeController + debut de commande (test) test

--- src/main/java/controllers/StudentHomeController.java
+++ src/main/java/controllers/StudentHomeController.java
@@ -1,14 +1,17 @@
 package controllers;
 
+import dao.StudentDAO;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableView;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import models.Student;
 
 import java.net.URL;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
 
 public class StudentHomeController {
@@ -21,15 +24,18 @@ public class StudentHomeController {
 
     @FXML
     private ChoiceBox groupChoiceBox;
-
     @FXML
     private TableView listEtuTableView;
 
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    Student student = new Student();
 
-        groupChoiceBox.getItems().add("Promo");
-        groupChoiceBox.getItems().add("TD");
-        groupChoiceBox.getItems().add("(TP");
+    public void initialize(Stage stage) {
+        stage.setTitle("test");
+
+        studentNameText.setText(student.getName());
+
+        groupChoiceBox.getItems().addAll("Promo","TD","TP");
+
     }
 
 
