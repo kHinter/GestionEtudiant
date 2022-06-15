229ccb1548d63082e0eb07d60ec02b71ba42d293
DarkLegume <pacopas1512@gmail.com>
2022-06-15 11:46:15 +0200

    Fonction d'ajout de groupe

--- src/main/java/controllers/secretary/AddNewGroupFormController.java
+++ src/main/java/controllers/secretary/AddNewGroupFormController.java
@@ -1,16 +1,23 @@
 package controllers.secretary;
 
+import dao.GroupDAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import models.Group;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
-public class AddNewGroupFormController {
+public class AddNewGroupFormController extends SecretaryController{
 
     @FXML
     private Button cancelButton, confirmButton;
+
+    @FXML
+    private TextField groupName;
 
     public void onCancelClicked(ActionEvent actionEvent) {
 
@@ -20,7 +27,35 @@ public class AddNewGroupFormController {
 
     public void onConfirmClicked(ActionEvent actionEvent) throws IOException {
 
-        // Ajout du groupe
+        Group group = new Group();
+        GroupDAO groupDAO = new GroupDAO();
+
+        group.setId(groupName.getText());
+
+        String currentType = groupType(getCurrentGroup().getType());
+        group.setType(currentType);
+
+        group.setParent(getCurrentGroup());
+
+        try {
+            groupDAO.save(group);
+        } catch (SQLException e) {
+            System.out.println("Impossible d'insérer le groupe "+group.getId());
+        }
     }
 
+    @Override
+    public void init() {
+
+    }
+
+    public String groupType(String parentType){
+        if(parentType.equals("PROMOTION")){
+            return "TD";
+        }
+        else if(parentType.equals("TD")){
+            return "TP";
+        }
+        return "PROMOTION";
+    }
 }
